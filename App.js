0ec5a091b874ecbca34b731bd62b1b56aaa0817c
francofonceca <57695672+francofonceca@users.noreply.github.com>
2020-04-02 12:37:48 -0300

    Archivos js y css que inclui en el App.js
    Adobe me dio el diseño web entero con el js el css y el html en un mismo archivo, los ordene y inclui en App.js.
    100% Funcional.
    Primer intento de diseño con AdobeXD y REACT.JS

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Prueba from 'pruebahtml';
+import Wea from 'pruebahtml';
+import 'Prueba.css';
+  class App extends Component{
+
+
+
+    render() {
+      return(
+        <div className="App">
+          <Prueba />
+          <Wea />
+        </div>
+
+      );
+    }
+  }
+export default App;
---
+++ pruebahtml.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+
+class Prueba extends Component {
+  render (){
+    return(
+
+      <div id="Blog">
+          <div id="Titulo">
+            <div id="Blog_i">
+              <span>Blogs</span>
+            </div>
+            <svg class="L_nea_1" viewBox="0 0 213 4">
+              <path fill="transparent" stroke="rgba(255,170,58,1)" stroke-width="4px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_1" d="M 0 0 L 213 0">
+              </path>
+            </svg>
+            <svg class="L_nea_2" viewBox="0 0 213.5 4">
+              <path  sfill="transparent" stroke="rgba(255,170,58,1)" stroke-width="4px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_2" d="M 0 0 L 213.5 0">
+              </path>
+            </svg>
+          </div>
+
+          <div id="Articulo">
+
+              <div id="Articulo_m">
+                <img id="IMG_2232" src="https://media.staticontent.com/media/pictures/9845587c-21d7-4822-be21-bc30fcc47836" srcset="IMG_2232.png 12x, IMG_2232@2x.png 2x"></img>
+                <div id="Paisaje_tur_stico_2019">
+                  <span>Paisaje turístico 2019</span>
+                </div>
+                <div id="Bariloche__en_la_Patagonia__mu">
+                  <span>Bariloche, en la Patagonia. mucho frio, a veces no tanto pero todo muy lindo por donde lo veas.</span>
+                </div>
+                <svg class="L_nea_3" viewBox="0 0 435 4">
+                  <path fill="transparent" stroke="rgba(113,109,103,1)" stroke-width="4px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_3" d="M 0 0 L 435 0">
+                  </path>
+                </svg>
+              </div>
+
+              <div id="Articulo_r">
+                <img id="IMG_2232_s" src="https://media.staticontent.com/media/pictures/9845587c-21d7-4822-be21-bc30fcc47836" srcset="IMG_2232_s.png 12x, IMG_2232_s@2x.png 2x"></img>
+                <div id="Paisaje_tur_stico_2019_t">
+                  <span>Paisaje turístico 2019</span>
+                </div>
+                <div id="Bariloche__en_la_Patagonia__mu_u">
+                  <span>Bariloche, en la Patagonia. mucho frio, a veces no tanto pero todo muy lindo por donde lo veas.</span>
+                </div>
+                <svg class="L_nea_3_v" viewBox="0 0 435 4">
+                  <path fill="transparent" stroke="rgba(113,109,103,1)" stroke-width="4px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_3_v" d="M 0 0 L 435 0">
+                  </path>
+                </svg>
+              </div>
+            <div id="Articulo_w">
+              <img id="IMG_2232_x" src="https://media.staticontent.com/media/pictures/9845587c-21d7-4822-be21-bc30fcc47836" srcset="IMG_2232_x.png 12x, IMG_2232_x@2x.png 2x"></img>
+              <div id="Paisaje_tur_stico_2019_y">
+                <span>Paisaje turístico 2019</span>
+              </div>
+              <div id="Bariloche__en_la_Patagonia__mu_z">
+                <span>Bariloche, en la Patagonia. mucho frio, a veces no tanto pero todo muy lindo por donde lo veas.</span>
+              </div>
+              <svg class="L_nea_3_" viewBox="0 0 435 4">
+                <path fill="transparent" stroke="rgba(113,109,103,1)" stroke-width="4px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_3_" d="M 0 0 L 435 0">
+                </path>
+              </svg>
+            </div>
+            <div id="Articulo_">
+              <img id="IMG_2232_" src="https://media.staticontent.com/media/pictures/9845587c-21d7-4822-be21-bc30fcc47836" srcset="IMG_2232_.png 12x, IMG_2232_@2x.png 2x"></img>
+              <div id="Paisaje_tur_stico_2019_">
+                <span>Paisaje turístico 2019</span>
+              </div>
+              <div id="Bariloche__en_la_Patagonia__mu_">
+                <span>Bariloche, en la Patagonia. mucho frio, a veces no tanto pero todo muy lindo por donde lo veas.</span>
+              </div>
+              <svg class="L_nea_3_ba" viewBox="0 0 435 4">
+                <path fill="transparent" stroke="rgba(113,109,103,1)" stroke-width="4px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="L_nea_3_ba" d="M 0 0 L 435 0">
+                </path>
+              </svg>
+            </div>
+          </div>
+      </div>
+
+    )
+  }
+}
+//exporto el metodo o etiqueta para poder incluirla en el App
+export default Prueba;
